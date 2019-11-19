88e8c7cdf2840da5531f33047fb516c0bf5849c3
angelabenavente <angela.b.almazan@gmail.com>
2019-11-19 18:24:38 +0100

    Add js function to change face

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -1,3 +1,16 @@
 'use strict';
 
-console.log('>> Ready :)');
+const buttonElement = document.querySelector('#update-button');
+const selectElement = document.querySelector('#mood');
+const faceElement = document.querySelector('#face');
+
+const random = Math.random();
+console.log(Math.round(random));
+
+const updateMood = () => {
+  faceElement.innerHTML = selectElement.value;
+
+}
+
+buttonElement.addEventListener('click', updateMood);
+console.log('hola');
